7ada7d6ade82c0474dd847e91211aa115c2e2f7b
Ritika Rawat <Ritikarawat220@gmail.com>
2023-06-20 17:30:29 +0530

    update changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
   has_one_attached :photo
-  has_many :likes, foreign_key: 'author_id' # specify the custom foreign key name
+  has_many :likes, foreign_key: 'author_id'
 end
