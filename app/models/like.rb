57a8445f4dd59c748e98befe84e309706f5ce91d
Ritika Rawat <Ritikarawat220@gmail.com>
2023-06-20 17:20:39 +0530

    Fix linter errors

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,4 @@
 class Like < ApplicationRecord
-    belongs_to :user, class_name: 'User', foreign_key: 'author_id' # specify the custom foreign key name
-    belongs_to :post
-  end
+  belongs_to :user, class_name: 'User', foreign_key: 'author_id' # specify the custom foreign key name
+  belongs_to :post
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,3 @@
 class Post < ApplicationRecord
-    belongs_to :user, class_name: 'User', foreign_key: 'author_id'
-  end
-  
+  belongs_to :user, class_name: 'User', foreign_key: 'author_id'
+end
