9e0468a305c077422ea3f981f35934bcfa785608
Ritika Rawat <Ritikarawat220@gmail.com>
2023-06-20 21:21:36 +0530

    Update changes as per review

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_20_114821) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_20_154752) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,6 +47,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_20_114821) do
     t.integer "posts_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo"
   end
 
   add_foreign_key "comments", "posts"
