5bc5252b5fa38faecec1b1302125c44d99e13c2f
Ritika Rawat <Ritikarawat220@gmail.com>
2023-06-21 15:27:36 +0530

    Add method to update the users, posts, comments, likes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,18 @@
 class Post < ApplicationRecord
-  belongs_to :user, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  has_many :comments
+  has_many :likes
+
+  after_create :update_user_posts_counter
+  after_destroy :update_user_posts_counter
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
+  private
+
+  def update_user_posts_counter
+    author.update(posts_counter: author.posts.count) # Consider changing to user
+  end
 end
