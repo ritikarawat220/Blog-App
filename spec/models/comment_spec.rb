701aa5a219867760372433f7c04d0f0b3534789e
Ritika Rawat <Ritikarawat220@gmail.com>
2023-06-22 17:23:19 +0530

    Add tests for comment

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  describe 'Validations' do
+    let(:user) do
+      User.create(name: 'Ritika', photo: 'Image will be displayed here', bio: 'Hello pals', posts_counter: 0)
+    end
+    let(:post) do
+      Post.create(title: 'First Post', text: 'This is my first post', author_id: user.id, comments_counter: 0,
+                  likes_counter: 0)
+    end
+
+    subject(:comment) do
+      described_class.create(text: 'This is my first comment', post: post, author: user)
+    end
+
+    it 'is valid without text' do
+      subject.text = nil
+      expect(subject).to be_valid
+    end
+
+    it 'increments the comments counter of the associated post' do
+      expect { comment.save }.to change { post.reload.comments_counter }.by(1)
+    end
+  end
+end
