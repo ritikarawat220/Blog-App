e9220f677dde3dc405091cbad6467518e588810f
Ritika Rawat <Ritikarawat220@gmail.com>
2023-06-22 17:07:53 +0530

    Add testing for users

--- Gemfile
+++ Gemfile
@@ -56,8 +56,9 @@ end
 
 group :development do
   # Use console on exceptions pages [https://github.com/rails/web-console]
+  gem 'rspec'
   gem 'rspec-rails'
-  gem 'web-console'
+  gem 'shoulda-matchers', '~> 4.0'
 
   # Add speed badges [https://github.com/MiniProfiler/rack-mini-profiler]
   # gem "rack-mini-profiler"
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  let(:user) { User.create(name: 'Ritika', photo: 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExMjk2ODdiNGFkMzE3ODQ1NDFkOTdhYWVhZWYxMDYxNjdhMWVhM2JlOCZlcD12MV9pbnRlcm5hbF9naWZzX2dpZklkJmN0PWc/BG9OrtFcxH9CjNpNcT/giphy.gif', bio: 'Hey there', posts_counter: 0) }
+
+  it 'Name must not be nil' do
+    user.name = nil
+    expect(user).to_not be_valid
+  end
+
+  it 'Post counter number must be numeric' do
+    user.posts_counter = ''
+    expect(user).to_not be_valid
+  end
+
+  it 'Post counter number must be greater than or equal to 0' do
+    user.posts_counter = -10
+    expect(user).to_not be_valid
+  end
+
+  describe '#recent_posts' do
+    before do
+      4.times do |i|
+        Post.create(title: "Post #{i + 1}", text: "This is post #{i + 1}",
+                    author_id: user.id, comments_counter: 0,
+                    likes_counter: 0)
+      end
+    end
+
+    it 'should return 3 most recent posts' do
+      expect(user.recent_posts.length).to eq(3)
+    end
+
+    it 'should return most recent one as first item' do
+      expect(user.recent_posts.first.title).to eq('Post 4')
+    end
+  end
+end
