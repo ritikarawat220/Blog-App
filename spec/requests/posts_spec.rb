33c9c0baf09657567db11ae4381e148cfc85b25e
Ritika Rawat <Ritikarawat220@gmail.com>
2023-06-28 13:45:34 +0530

    Update test for post

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,16 +2,28 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
-    it 'returns http success' do
+    it 'returns http success and renders the index template' do
       get '/users/:user_id/posts'
       expect(response).to have_http_status(:success)
+      expect(response).to render_template(:index)
+    end
+
+    it 'includes correct placeholder text in the response body' do
+      get '/users/:user_id/posts'
+      expect(response.body).to match('Here is a list of posts and comments for a given user')
     end
   end
 
   describe 'GET /show' do
-    it 'returns http success' do
+    it 'returns http success and renders the show template' do
       get '/users/:user_id/posts/:id'
       expect(response).to have_http_status(:success)
+      expect(response).to render_template(:show)
+    end
+
+    it 'includes correct placeholder text in the response body' do
+      get '/users/:user_id/posts/:id'
+      expect(response.body).to match('Here is a post and its list of comments')
     end
   end
 end
