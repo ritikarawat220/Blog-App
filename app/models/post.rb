9276fcf9096be9bb9f14b955ae215b544706b855
Ritika Rawat <Ritikarawat220@gmail.com>
2023-07-10 15:54:43 +0530

    Fix error

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,8 +7,8 @@ class Post < ApplicationRecord
   after_destroy :update_user_posts_counter
 
   validates :title, presence: true, length: { maximum: 250 }
-  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_nil: true
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_nil: true
 
   def recent_comments
     comments.order(created_at: :desc).limit(5)
