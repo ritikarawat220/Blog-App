e331ecca4a6780e17bda3940798c30fd014fbf9e
Ritika Rawat <Ritikarawat220@gmail.com>
2023-07-10 13:55:36 +0530

    Fix n+1 problem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_06_081829) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_10_081201) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
