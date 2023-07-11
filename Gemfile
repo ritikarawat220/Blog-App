5af9b6e125b93a7aecc9fd93f04e6d47fc10f2d7
Ritika Rawat <Ritikarawat220@gmail.com>
2023-07-11 17:49:24 +0530

    Add sig-np

--- Gemfile
+++ Gemfile
@@ -79,4 +79,4 @@ group :development, :test do
   gem 'shoulda-matchers'
 end
 
-gem "devise", "~> 4.9"
+gem 'devise', '~> 4.9'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+    # before_action :authenticate_user!
+  protect_from_forgery with: :exception
+  before_action :update_allowed_parameters, if: :devise_controller?
+  def after_sign_in_path_for(_resource)
+    users_path
+  end
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:email, :name, :photo, :bio, :password, :password_confirmation)
+    end
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:email, :name, :photo, :bio, :password, :current_password, :password_confirmation)
+    end
+  end
 end
