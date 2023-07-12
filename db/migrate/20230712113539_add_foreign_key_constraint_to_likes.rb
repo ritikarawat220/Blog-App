0067a549de573cdc5ac65cb9c94fdf6d087129aa
Ritika Rawat <Ritikarawat220@gmail.com>
2023-07-12 17:10:52 +0530

    Add Foreign KeyConstraint

---
+++ db/migrate/20230712113539_add_foreign_key_constraint_to_likes.rb
@@ -0,0 +1,4 @@
+class AddForeignKeyConstraintToLikes < ActiveRecord::Migration[7.0]
+  def change
+  end
+end
---
+++ db/migrate/20230712113656_add_foreign_key_constraint_to_comments.rb
@@ -0,0 +1,4 @@
+class AddForeignKeyConstraintToComments < ActiveRecord::Migration[7.0]
+  def change
+  end
+end
