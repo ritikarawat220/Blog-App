73a97e2e315f99181514a3e803db07b61ecc7855
Mohammad Mohsen Haidari <mohsenhaidari0766@gmail.com>
2023-07-13 12:34:08 +0430

    routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   # root to: 'home#index'
   root 'users#index'
   resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show new create] do
+    resources :posts, only: %i[index show new create destroy] do
       resources :comments, only: %i[new create destroy]
       resources :likes, only: [:create]
     end
