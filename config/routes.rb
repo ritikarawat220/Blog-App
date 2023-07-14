9c1a5cb392c589da818b146e5647da24f74d6c64
Ritika Rawat <Ritikarawat220@gmail.com>
2023-07-14 14:31:09 +0530

    Remove commented lines of code

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,8 @@
 Rails.application.routes.draw do
   get 'sessions/new'
   get 'sessions/create'
-
-  # devise_for :users
   devise_for :users, controllers: { sessions: 'users/sessions', passwords: 'devise/passwords' }
-  
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-  # root to: 'home#index'
   root 'users#index'
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create, :destroy] do
@@ -19,7 +12,6 @@ Rails.application.routes.draw do
   end
   get '/sign_out_user', to: 'users#sign_out_user', as: 'sign_out_user'
 
-  # API routes
   namespace :api do
     namespace :v1 do
       resources :users, only: [:index, :show] do
